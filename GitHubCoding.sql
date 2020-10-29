6eb3aef0834e63807def02166cd9a73d6f7df599
Kavinskay <kevinseegers123@gmail.com>
2020-10-29 15:05:30 +0100

    updating coding files

--- GitHubCoding.sql
+++ GitHubCoding.sql
@@ -1,33 +1,33 @@
-#### Setting up and linking a github 
+-- Setting up and linking a github 
 
-####installing Gedit (needed for this assignment)
+-- installing Gedit (needed for this assignment)
 pi@raspberrypi:~/GitHub $ sudo apt-get install gedit
 
-####making new directory 
+-- making new directory 
 pi@raspberrypi:~ $ mkdir hubspot
 
-####open directory
+-- open directory
 pi@raspberrypi:~ $ cd hubspot
 
-####create repo 
+-- create repo 
 pi@raspberrypi:~/hubspot $ git init
 Initialized empty Git repository in /home/pi/hubspot/.git/
 
-####linking to user 
+-- linking to user 
 pi@raspberrypi:~/hubspot $ git config --global user.mail "kevinseegers123@gmail.com"
 pi@raspberrypi:~/hubspot $ git config --global user.name "Kavinskay"
 
-####checking for connection
+-- checking for connection
 pi@raspberrypi:~/hubspot $ ls -a
 .  ..  .git
 
-####make and edit file 
+-- make and edit file 
 pi@raspberrypi:~/hubspot $ gedit README.md
 
-####Adding file to the waiting room
+-- Adding file to the waiting room
 pi@raspberrypi:~/hubspot $ git add README.md
 
-####overview of files waiting to be comitted 
+-- overview of files waiting to be comitted 
 pi@raspberrypi:~/hubspot $ git status
 On branch master
 
@@ -38,13 +38,13 @@ Changes to be committed:
 
 	new file:   README.md
 
-#####comitting readme file to the repo skeleton
+-- comitting readme file to the repo skeleton
 pi@raspberrypi:~/hubspot $ git commit -am "add README.md"
 [master (root-commit) b30b8c0] add README.md
  1 file changed, 3 insertions(+)
  create mode 100644 README.md
 
-####adding other files to the repo skeleton
+-- adding other files to the repo skeleton
 pi@raspberrypi:~/hubspot $ git add --all
 pi@raspberrypi:~/hubspot $ git commit -am "uploading recommender system files"
 [master fb67a04] uploading recommender system files
@@ -55,7 +55,7 @@ pi@raspberrypi:~/hubspot $ git commit -am "uploading recommender system files"
  create mode 100644 Reccomender system /top50reccomendationsforSocialNetwork.csv
  create mode 100644 Reccomender system /topreccomendationsStarringforSocialNetwork.csv
 
-####practise log function
+-- practise log function
 pi@raspberrypi:~/hubspot $ git log README.md
 commit b30b8c029d0d29fa98ef079c5833540465b0012d
 Author: Kavinskay <kevinseegers123@gmail.com>
@@ -68,37 +68,37 @@ fatal: ambiguous argument 'RecommenderCoding.sql': unknown revision or path not 
 Use '--' to separate paths from revisions, like this:
 'git <command> [<revision>...] -- [<file>...]'
 
-####checking current repo status 
+-- checking current repo status 
 pi@raspberrypi:~/hubspot $ git status
 On branch master
 nothing to commit, working tree clean
 
-####making branche 
+-- making branche 
 pi@raspberrypi:~/hubspot $ git checkout -b lidar-version 
 Switched to a new branch 'lidar-version'
 
-####checking branche 
+-- checking branche 
 pi@raspberrypi:~/hubspot $ git branch
 * lidar-version
   master
 
-####changing to master and merging the branches 
+-- changing to master and merging the branches 
 pi@raspberrypi:~/hubspot $ git checkout master
 Switched to branch 'master'
 pi@raspberrypi:~/hubspot $ git merge lidar-version
 
-####setting up and checking connection to the cloud 
+-- setting up and checking connection to the cloud 
 pi@raspberrypi:~/hubspot $ git remote add origin kevinseegers123@gmail.com:Kavinskay/hubspot.git
 pi@raspberrypi:~/hubspot $ git remote -v
 origin	kevinseegers123@gmail.com:Kavinskay/hubspot.git (fetch)
 origin	kevinseegers123@gmail.com:Kavinskay/hubspot.git (push)
 
-####Removing a origin (when needed)
+-- Removing a origin (when needed)
 pi@raspberrypi:~/hubspot $ git remote rm origin
 #Checking for sure
 pi@raspberrypi:~/hubspot $ git remote -v
 
-####pushing first files 
+-- pushing first files 
 pi@raspberrypi:~/hubspot $ git push -u origin master
 Username for 'https://github.com': Kavinskay
 Password for 'https://Kavinskay@github.com': 
